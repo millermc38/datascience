2310f0fe4a68db8ebdf64709a87456af8ecbf113
millermc38 <millermc38@gmail.com>
2020-09-09 14:11:58 -0400

    Commit n

--- master.R
+++ master.R
@@ -1,6 +1,3 @@
-devtools::install_github(repo = "millermc38/datascience",
-                         ref = "master",
-                         auth_token = "e60a504605e884f88d4733b5ea756b36472a4fdb")
-usethis::use_description()
-
-create_package("C:/Users/Mark/Documents/Coding Practice/datascience")
+foofun<-function(x){
+  1+1
+}
