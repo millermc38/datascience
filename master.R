9a944e2a95ab65ea644e558673ccfda64b3d1cd5
millermc38 <millermc38@gmail.com>
2020-09-09 17:17:42 -0400

    Adding some new files

---
+++ master.R
@@ -0,0 +1,11 @@
+library(usethis)
+use_package("caret",type="import")
+use_package("asbio",type="import")
+
+
+devtools::install_github("millermc38/datascience",force = T,upgrade = "never")
+
+
+library(datascience)
+korea_is()
+
