3248a39debb6f98a832b388142b7cc721ec1cab4
millermc38 <millermc38@gmail.com>
2020-09-09 13:46:23 -0400

    A new commit

--- master.R
+++ master.R
@@ -1,2 +1,3 @@
-library(devtools)
-use_r("foofun")
+devtools::install_github(repo = "millermc38/datascience",
+                         ref = "master",
+                         auth_token = "e60a504605e884f88d4733b5ea756b36472a4fdb")
