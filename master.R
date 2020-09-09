a7b9b21e9ebba67605a0753cefc209963896bf6c
millermc38 <millermc38@gmail.com>
2020-09-09 13:54:15 -0400

    Trying to initialize package

--- master.R
+++ master.R
@@ -1,3 +1,6 @@
 devtools::install_github(repo = "millermc38/datascience",
                          ref = "master",
                          auth_token = "e60a504605e884f88d4733b5ea756b36472a4fdb")
+usethis::use_description()
+
+create_package("C:/Users/Mark/Documents/Coding Practice/datascience")
