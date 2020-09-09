eb181e1ab7920951b7a6542824eab37dce18ebd2
millermc38 <millermc38@gmail.com>
2020-09-09 17:23:25 -0400

    New

--- master.R
+++ master.R
@@ -1,11 +1,10 @@
-library(usethis)
-use_package("caret",type="import")
-use_package("asbio",type="import")
-
-
-devtools::install_github("millermc38/datascience",force = T,upgrade = "never")
-
-
-library(datascience)
-korea_is()
-
+# library(usethis)
+# use_package("caret",type="import")
+# use_package("asbio",type="import")
+#
+#
+# devtools::install_github("millermc38/datascience",force = T,upgrade = "never")
+#
+#
+# library(datascience)
+#
