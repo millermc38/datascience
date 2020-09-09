4ffb661428da02a5f622437e23184ae710e74f61
millermc38 <millermc38@gmail.com>
2020-09-09 17:27:05 -0400

    New updates

--- R/ds.R
+++ R/ds.R
@@ -1,4 +1,4 @@
-#' @export
+#' @export datascience
 datascience<-function(data, response, covariates,rounding,family,parameter){
   ########################################################### SETUP
 
